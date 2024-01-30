a821c3126360ed8e408f1e5fd4d43dd24a1c493b
caterbob <trickshotkoopa2007@gmail.com>
2024-01-29 19:26:08 -0800

    Climber basic superstructure

--- src/main/java/com/team957/comp2024/subsystems/boxClimber/BoxClimber.java
+++ src/main/java/com/team957/comp2024/subsystems/boxClimber/BoxClimber.java
@@ -1,38 +1,44 @@
 package com.team957.comp2024.subsystems.boxClimber;
 
-import java.util.function.Supplier;
+import com.team957.comp2024.Constants;
+import com.team957.comp2024.Constants.ClimberConstants;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import monologue.Logged;
 
 public abstract class BoxClimber implements Subsystem, Logged{
-    public abstract void setMotorVoltage();
+    public abstract void setMotorVoltage(double voltage);
 
     public abstract double getMotorVoltage();
 
     public abstract double getMotorAmps();
 
-    public abstract void setSetpoint(double meters);
-
-    public abstract double getPosition();
-
-    public abstract double getVelocity();
-
-    public abstract double getAcceleration();
-
-    public Command goToSetSetpoint(Supplier<Double> position){
+    // puts the climber in rest
+    public Command idleCommand(){
         return run(
             () -> {
+                setMotorVoltage(0);
+            }
+        );
+    }
 
+    // raises the climber
+    public Command raiseCommand(){
+        return run(
+            () -> {
+                setMotorVoltage(ClimberConstants.STANDARD_VOLTAGE);
+            }
+        );
+    }
+
+    // lowers the climber
+    public Command lowerCommand(){
+        return run(
+            () -> {
+                setMotorVoltage(-ClimberConstants.STANDARD_VOLTAGE);
             }
         );
     }
 }
-
-
-
-
-
-
 //miles was here
---
+++ src/main/java/com/team957/comp2024/subsystems/boxClimber/BoxClimberSim.java
@@ -0,0 +1,5 @@
+package com.team957.comp2024.subsystems.boxClimber;
+
+public class BoxClimberSim {
+    
+}
