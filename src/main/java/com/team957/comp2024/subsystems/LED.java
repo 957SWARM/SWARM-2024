633e7b808545355ffc0edf62e8db8cd43d408b35
caterbob <trickshotkoopa2007@gmail.com>
2024-02-05 14:21:40 -0800

    WIP LED Segmentation code.

--- src/main/java/com/team957/comp2024/subsystems/LED.java
+++ src/main/java/com/team957/comp2024/subsystems/LED.java
@@ -2,6 +2,7 @@ package com.team957.comp2024.subsystems;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -9,6 +10,7 @@ public class LED implements Subsystem{
 
     private AddressableLED m_led;
     private AddressableLEDBuffer m_ledBuffer;
+    private AddressableLEDBuffer m_ledSegment;
 
     public LED(){
         m_led = new AddressableLED(0); 
@@ -19,6 +21,9 @@ public class LED implements Subsystem{
         // Set the data
         m_led.setData(m_ledBuffer);
         m_led.start();
-        
+    }
+
+    public Command Segmentation() {
+        this.runOnce(() -> m_ledSegment = m_ledSegment.get(m_ledBuffer));
     }
 }
