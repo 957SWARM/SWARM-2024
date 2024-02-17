75ef217ab332a019baaaa7b09e59fd1ec74c8557
Jerry...or is it? <guoguo1813@gmail.com>
2024-02-17 14:42:04 -0800

    Some restructuring in progress.

---
+++ src/main/java/com/team957/comp2024/subsystems/LED/LED.java
@@ -0,0 +1,11 @@
+package com.team957.comp2024.subsystems.LED;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+public class LED implements Subsystem{
+    
+    public LED(){
+        register();
+    }
+
+}
