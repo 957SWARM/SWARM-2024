2252e902c09b1b7bb3c7e26090236d40631f43ea
Jerry...or is it? <guoguo1813@gmail.com>
2024-02-17 16:38:26 -0800

    Honestly I have no idea what I'm doing but now I'm starting to understand.

--- src/main/java/com/team957/comp2024/subsystems/LED/LED.java
+++ src/main/java/com/team957/comp2024/subsystems/LED/LED.java
@@ -1,7 +1,10 @@
 package com.team957.comp2024.subsystems.LED;
 
+import java.lang.invoke.LambdaConversionException;
+
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class LED implements Subsystem{
@@ -9,6 +12,11 @@ public class LED implements Subsystem{
     private AddressableLED m_led;
     private AddressableLEDBuffer m_ledBuffer;
     //private AddressableLEDBuffer m_ledSegment;
+
+    int Pixel;
+    int startPixel;
+    int endPixel;
+    int totalPixels;
     
     public LED(){
         register();
@@ -17,9 +25,22 @@ public class LED implements Subsystem{
 
         m_ledBuffer = new AddressableLEDBuffer(0);
         m_led.setLength(m_ledBuffer.getLength());
+        
+        int totalPixels = m_ledBuffer.getLength();
 
         // Set the data
         m_led.setData(m_ledBuffer);
         m_led.start();
+    }
+
+    /*public int startPixel(int totalPixels){
+        return totalPixels;
+    }*/
+    
+    public void setStartAndEndPixel(){
+        //For Loop to first define Start and End Pixels
+        for (int i = 0; i <= totalPixels; i++){
+            
+        }
     }
 }
